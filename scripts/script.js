f51bef6648417aa70196b2ba5cd6f1550665b8db
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2025-01-18 16:27:30 -0500

    start of game + flipping channels

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
-var textArray, textPos = 0;
-var textBox;
+var textArray, textPos = 0, textBox;
+var tvChannels, currChannel, channelPos;
+var channelOn = false;
 
 function setup() {
     createCanvas(700, 700);
+    allSprites.rotationLock = true;
     textArray = [
         "Hmmm....I wonder what's on TV today.",
         "Oh...this show. It's not like I have anything better to watch.",
@@ -11,24 +13,61 @@ function setup() {
     textBox = new Sprite(width - (width / 2), height - (height / 4), width / 2, height / 4);
     textBox.color = "white";
     textBox.text = textArray[textPos];
+    tvChannels = [
+        "DisKnee Channel", "DisKnee Junior", "Rockola", "Pop TV"
+    ];
+    do {
+        currChannel = random(tvChannels);
+    } while (currChannel == "Rockola");
+    channelPos = tvChannels.indexOf(currChannel);
+    console.log(tvChannels);
 }
 
 function draw() {
     background("#b00b69");
+    text("Use the arrow keys to change the channel to Rockola!", width / 3 - 15, 15);
+    text(`Channel: ${currChannel}`, width / 2, height / 2);
+    channelOn = (currChannel == "Rockola") ? true : false;
+}
+
+function keyPressed() {
+    if (!channelOn) {
+        switch(keyCode) {
+            case UP_ARROW:
+                currChannel = tvChannels[++channelPos];
+                if (channelPos >= tvChannels.length) {
+                    channelPos = 0;
+                    currChannel = tvChannels[channelPos];
+                }
+                break;
+            case DOWN_ARROW:
+                currChannel = tvChannels[--channelPos];
+                if (channelPos < 0) {
+                    channelPos = tvChannels.length - 1;
+                    currChannel = tvChannels[channelPos];
+                }
+                break;
+            default:
+                console.log(keyCode);
+        }
+    }
 }
 
 function mousePressed() {
-    try {
-        if(textPos + 1 > textArray.length) {
-            throw new RangeError();
+    if (channelOn) {
+        try {
+            if(textPos + 1 > textArray.length) {
+                throw new RangeError();
+            }
+            textBox.text = textArray[++textPos];
         }
-        textBox.text = textArray[++textPos];
-    }
-    catch(RangeError) {
-        console.log("No more text to cycle through.");
-    }
-    finally {
-        if (textPos >= textArray.length)
-            textBox.text = textArray[textArray.length - 1];
+        catch(RangeError) {
+            console.log("No more text to cycle through.");
+        }
+        finally {
+            if (textPos >= textArray.length) {
+                textBox.text = textArray[textArray.length - 1];
+            }
+        }
     }
 }
